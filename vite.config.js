7a24d9f12539f1a1200fb9deb90cf306e50013ad
Ahmad Kholid <Kholid060@gmail.com>
2021-07-31 17:55:01 +0800

    feat: add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { join } from 'path';
+import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    {
+      ...eslint({
+        include: ['./src/**/*.vue', './src/**/*.js'],
+      }),
+      enforce: 'pre',
+    },
+  ],
+  resolve: {
+    alias: {
+      '@/': join(__dirname, 'src/'),
+    },
+  },
+});
